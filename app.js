1e50575d416d3fa8150d3b4bc9185d212039ad96
Mayur kumar <mk.282@live.in>
2021-11-29 20:13:49 +0530

    Added hello route

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express=require('express');
 const app=express();
 
 app.get('/',function(req, res){
-    res.json('Hi');
+    res.json('App is running');
+});
+
+app.get('/hello',function(req, res){
+    res.json('Hello');
 });
 
 app.listen(80);
